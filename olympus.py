fdb8177847bd6407f52ebff41413a958890ead73
Quantum Snowball <quantum.snowball@gmail.com>
2021-12-01 19:13:20 +0800

    refactor: just olympus

--- olympus.py
+++ olympus.py
@@ -94,3 +94,23 @@ class Olympus:
             f'Profit=${self._epoch_profit:,.0f}, '
             f'Mint={self._epoch_minted:,.2f} OHM'
         ))
+
+
+def main():
+    ohm = Olympus(supply=1_000_000)
+
+    N = 30
+    for epoch in range(1, N+1):
+        # market
+        with ohm(epoch, price=100.0) as project:
+            # simulate the bonding amount
+            amount = (N-epoch+1)/N*0.25 * ohm.treasury
+            # someone do 1,1 (bonding)
+            project.bond(amount)
+
+            # assume everybody 3,3 (staking)
+            pass
+
+
+if __name__ == '__main__':
+    main()
